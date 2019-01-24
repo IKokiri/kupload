aad884942bbc6fda0c88c26f8f8269a140c700e2
Luiz Mendes <developer.luizmendes@gmail.com>
2019-01-24 10:39:10 -0200

    remoção load balancer

--- link/index.php
+++ link/index.php
@@ -1,11 +1,3 @@
 <?php
 
-$host="10.0.0.252";
-
-exec("ping -c 1 " . $host, $output, $result);
-
-if(isset($output[5])){
-    header("Location: http://10.0.0.252:8090/kupload/app/upload.php");
-}else{
     header("Location: http://201.49.127.157:9003/kupload/app/upload.php");
-}
